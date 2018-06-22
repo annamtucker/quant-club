ee46ac05976c33c88326ceab5dfdbf121e37f333
richardastantonjr <richardastantonjr@gmail.com>
2018-06-22 13:22:22 -0500

    Added commentary on my interpretation of the figures

--- Stanton_testing.R
+++ Stanton_testing.R
@@ -1,4 +1,6 @@
-## Plot honey production dataset
+## Plot honey production dataset to explore the relationship between
+## stocks and prices and whether the price volatility is mediated by stock 
+## volatility- it should be if the process is working.
 
 ## libraries
 library(tidyverse)
@@ -25,7 +27,10 @@ length(unique(honeyproduction$year))
 15*44 == nrow(honeyproduction)  ## Yup.
 15*44 - nrow(honeyproduction)   ## 34 state and year pairings are absent
 
-## How volatile are honey stocks and prices?
+## Here I got bored and decided the missing data probably don't matter 
+## for a casual exploration.
+
+## How volatile are honey stocks and prices among states and years?
 period_price_by_state <- group_by(honeyproduction, state) %>% 
   summarise(period_price = mean(priceperlb), period_sd = sd(priceperlb)) %>% 
   mutate(cv = period_sd/period_price)
@@ -111,12 +116,32 @@ state_level_plots <- plot_grid(st_prices, st_stocks, st_prices_cv, st_stocks_cv,
 annual_level_plots <- plot_grid(ann_prices, ann_stocks, ann_prices_cv, ann_stocks_cv,
           labels = c("A", "B", "C", "D"))
 
+## save as png with readable aspect ratios
+## The patterns here seem idiosyncratic so far, 
+## e.g. high prices states do not share high cost-of-living,
+## high stock states are CA, ND, SD
+## price volatility is consistent, HI is highest and well, it is the
+## most remote island on the planet
+## Stock volatility also makes no immediate sense and it probability 
+## a result of stochastic events. perhaps evident in the colony data
+## but I haven't looked. 
+
 save_plot("state_level_plots.png", state_level_plots,
           ncol = 2, # we're saving a grid plot of 2 columns
           nrow = 2, # and 2 rows
           # each individual subplot should have an aspect ratio of 1.3
           base_aspect_ratio = 3)
-          
+
+## Prices have increased while stocks are down.
+## Prices are ~1/3 as volatile as stocks, with no real fluctuation 
+## across years. The price increase appears modest but adjusting for 
+## inlfation would an obvious next step to see if honey stocks have
+## kept prices flat.
+
+## US Bureau of Labor Statistics inflation calculator 
+## https://www.bls.gov/data/inflation_calculator.htm
+## indicates that $1 in 1998 was worth $1.40 in 2012, so the price is
+## not completely flat, but pretty close.
 save_plot("annual_level_plots.png", annual_level_plots,
           ncol = 2, # we're saving a grid plot of 2 columns
           nrow = 2, # and 2 rows
